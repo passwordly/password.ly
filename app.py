b34ad4db8e59fcec4209ae3594f9d8d43a5bb705
ndorfin <ndorfin@gmail.com>
2012-11-17 17:53:12 +0200

    Wired up flask templates
    Using static data in the app/index.html

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 from flask import Flask, request, render_template
-
 from password import generatePassword
 
 app = Flask(__name__)
 
 @app.route('/')
-
-def index():
-  return "<form action='/generate' method='post'><input type='password' name='password' /><input type='text' name='site' /><input type='submit' /></form>"
+def index(name=None):
+  return render_template('index.html', name=name)
+#return "<form action='/generate' method='post'><input type='password' name='password' /><input type='text' name='site' /><input type='submit' /></form>"
 
 @app.route('/generate', methods=['POST'])
 def generate():
