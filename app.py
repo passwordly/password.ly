478f100b831131dbd2a6aa8610ea0f94ec079962
Josh Yudaken <j@yud.co.za>
2012-11-17 19:36:34 +0200

    Update with basic user support

--- app.py
+++ app.py
@@ -9,18 +9,25 @@ def index(name=None):
   return render_template('index.html', name=name)
 #return "<form action='/generate' method='post'><input type='password' name='password' /><input type='text' name='site' /><input type='submit' /></form>"
 
+@app.route('/<path:username>')
+def user(username):
+  params = {
+      'username': username
+  }
+  return render_template('user.html', **params)
+
 @app.route('/generate', methods=['POST'])
 def generate():
   password = str(request.form['password'])
   identifier = str(request.form['identifier'])
   
   result = generatePassword(password, identifier)
-  data = {
+  params = {
     'password': password,
     'identifier': identifier,
     'result': result
   }
-  return render_template('generate.html', **data)
+  return render_template('generate.html', **params)
 
 @app.route('/github-webhook')
 def github_hook():
