2e21547f1547dddb88be177d0ff7da55c4ade27e
Josh Yudaken <j@yud.co.za>
2012-11-17 16:31:14 +0200

    Add a basic flask app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from flask import Flask, request
+
+from password import generatePassword
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return "<form action='/generate' method='post'><input type='password' name='password' /><input type='text' name='site' /><input type='submit' /></form>"
+
+@app.route('/generate', methods=['POST'])
+def generate():
+  password = str(request.form['password'])
+  site = str(request.form['site'])
+  print password, site
+  return generatePassword(password, site)
+
+if __name__ == '__main__':
+  app.debug = True
+  app.run()
