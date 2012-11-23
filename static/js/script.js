ebdd2bccf9746f8dd03b6133aed8626f0db9befc
Josh Yudaken <j@yud.co.za>
2012-11-23 11:01:34 +0200

    Add start of the signup form

---
+++ static/js/script.js
@@ -0,0 +1,25 @@
+
+var passwordly = {};
+
+passwordly.autocompleteIdentifier = function(cb) {
+
+  $('#identifier').attr('autocomplete', 'off').typeahead({'source': cb});
+
+};
+
+$.fn.anychange = function (fn) {
+  $(this).change(fn).keyup(fn).each(fn);
+  return this;
+};
+
+$(function(){
+  if ($('#copy-result').length) {
+    ZeroClipboard.setMoviePath('static/swf/ZeroClipboard.swf');
+    var clip = new ZeroClipboard.Client();
+    clip.setText($('#result').val());
+    clip.glue('copy-result', 'copy-result-container');
+    $('#copy-result').click(function() {
+      alert('js failed');
+    });
+  }
+});
