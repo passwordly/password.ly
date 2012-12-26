ab2112b50c1ae0628acd87a213fbcd2d6455a5c4
Josh Yudaken <j@yud.co.za>
2012-12-26 14:15:29 +0700

    Minor style and html improvements

--- app.py
+++ app.py
@@ -199,9 +199,17 @@ def github_hook():
   os.system('git pull origin master')
   pass
 
-@app.route('/test')
-def test():
-  print User.signup(r, 'qix')
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('public/404.htm'), 404
+
+@app.errorhandler(500)
+def internal_error(e):
+    return render_template('public/500.htm'), 500
+
+@app.route('/test/exception')
+def test_exception():
+  raise Exception('oops')
 
 if __name__ == '__main__':
   app.debug = config.debug
